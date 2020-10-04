e2f1d65ded1e6fbe791bf6325c2e41b8dc5821a3
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-10-04 08:53:10 -0400

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright (C) Joseph R. Quinn
+// SPDX-License-Identifier: MIT
+
+pub trait PluMAPlugin<'a> {
+    fn input(&mut self, filepath: &'a str)-> Result<(), Box<dyn std::error::Error>>;
+    fn run(&self) -> Result<(), Box<dyn std::error::Error>>;
+    fn output(&self, filepath: &'a str) -> Result<(), Box<dyn std::error::Error>>;
+}
