b6017ac2e3eca95d419dd751418fb371c999410b
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-10-05 17:51:36 -0400

    Updating plugin trait for proper handling of &mut self in output

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@
 pub trait PluMAPlugin<'a> {
     fn input(&mut self, filepath: &'a str)-> Result<(), Box<dyn std::error::Error>>;
     fn run(&self) -> Result<(), Box<dyn std::error::Error>>;
-    fn output(&self, filepath: &'a str) -> Result<(), Box<dyn std::error::Error>>;
+    fn output(&mut self, filepath: &'a str) -> Result<(), Box<dyn std::error::Error>>;
 }
