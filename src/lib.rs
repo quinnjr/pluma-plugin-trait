f47fa3e30d7b626609487500612a7470b11c3c0b
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-10-23 19:32:50 -0400

    Re-adding Results to trait functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 pub trait PluMAPlugin {
-    fn input(&mut self, filepath: String);
-    fn run(&mut self);
-    fn output(&mut self, filepath: String);
+    fn input(&mut self, filepath: String) -> Result<(), Box<dyn std::error::Error>>;
+    fn run(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+    fn output(&mut self, filepath: String) -> Result<(), Box<dyn std::error::Error>>;
 }
